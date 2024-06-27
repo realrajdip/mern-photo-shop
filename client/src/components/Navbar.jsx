888a4d165731b4fc1c48ecf2a69d138fce1e9b73
Rajdip Bandopadhyay <rajdipbandopadhyay100@gmail.com>
2024-06-27 22:30:10 +0530

    image card component is done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,15 +4,24 @@ const Navbar = () => {
   return (
     <nav className="flex flex-col sm:flex-row justify-between items-start sm:items-center px-5 py-5 fixed top-0 left-0 right-0 shadow-md gap-1 sm:gap-0 z-30 bg-white">
       <div className="flex justify-between items-center">
+        <img src="/picprismlogo.png" alt="logo" className="w-[50px]" />
         <Link to={"/"} className="font-bold text-3xl">
           PicPrism
         </Link>
       </div>
-      <ul className="flex gap-5 text-lg text-gray-400">
-        <Link to={'/'} className="hover:text-black cursor-pointer sm:p-2">About</Link>
-        <Link to={'/'} className="hover:text-black cursor-pointer sm:p-2">Contact</Link>
-        <Link to={'/login'} className="hover:text-black cursor-pointer sm:p-2">Log In</Link>
-        <Link  to={'/signup'} className="hover:text-black cursor-pointer sm:p-2">Sign Up</Link>
+      <ul className="flex gap-5 text-lg text-gray-400 ml-5 sm:ml-0">
+        <Link to={"/"} className="hover:text-black cursor-pointer sm:p-2">
+          About
+        </Link>
+        <Link to={"/"} className="hover:text-black cursor-pointer sm:p-2">
+          Contact
+        </Link>
+        <Link to={"/login"} className="hover:text-black cursor-pointer sm:p-2">
+          Log In
+        </Link>
+        <Link to={"/signup"} className="hover:text-black cursor-pointer sm:p-2">
+          Sign Up
+        </Link>
       </ul>
     </nav>
   );
