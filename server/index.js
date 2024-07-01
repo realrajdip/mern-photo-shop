2cf9db312c8ca220c179b865f0b7ae067c36bc78
Rajdip Bandopadhyay <rajdipbandopadhyay100@gmail.com>
2024-07-01 22:50:23 +0530

    server added

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
-const express = require('express'); 
-require('dotenv').config();
-
+const express = require("express");
+require("dotenv").config();
+const authRoute = require("./routes/authRoutes");
+const { readdirSync } = require("fs");
 const app = express();
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send("<center><h1>Nice</h1></center>")
-})
+app.get("/", (req, res) => {
+  res.send("<center><h1>Nice</h1></center>");
+});
+// app.use('/api', authRoute)
+// console.log(readdirSync('./routes/')); // [ 'authRoutes.js' ]
+
+readdirSync("./routes/").map((route) => {
+  return app.use("/api", require(`./routes/${route}`));
+});
 
 app.listen(PORT, () => {
-    console.log(`Server listening to port: ${PORT}`);
-})
+  console.log(`Server listening to port: ${PORT}`);
+});
